58a1146f0a2ea54a225c31b9e579b5f78add49b7
unknown <jamienichols@codingbutter.com>
2021-09-08 01:04:43 -0400

    Updated Commit

--- DataSources/index.js
+++ DataSources/index.js
@@ -1,11 +1,10 @@
-const configs = require("../config.json");
 const OmdbAPI = require("./OmdbAPI");
 const TmdbAPI = require("./TmdbAPI");
 const TorrentSearch = require("./scrapers/TorrentSearch");
 module.exports = (config) => () => {
   return {
-    OmdbAPI: new OmdbAPI(configs.omdb),
-    TmdbAPI: new TmdbAPI(configs.tmdb),
+    OmdbAPI: new OmdbAPI(config.omdb),
+    TmdbAPI: new TmdbAPI(config.tmdb),
     TorrentSearch,
   };
 };
